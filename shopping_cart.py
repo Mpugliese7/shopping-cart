b6d74e181700155f09018bc2e51bec86e239cf7b
Mpugliese7 <65572796+Mpugliese7@users.noreply.github.com>
2020-06-07 09:29:18 -0400

    Print receipt at the end

--- shopping_cart.py
+++ shopping_cart.py
@@ -38,12 +38,16 @@ def to_usd(my_price):
 # Information capture section
 
 total_price = 0
+selected_ids = []
 
 while True:
     selected_id = input("Please input a product identifier: ")
     if selected_id == "DONE":
         break
     else:
+        selected_ids.append(selected_id)
+
+for selected_id in selected_ids:
         matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
         matching_product = matching_products[0]
         total_price = total_price + matching_product["price"]
